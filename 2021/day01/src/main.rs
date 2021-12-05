0d3675d7a1f9a796a90ce722a190a0ab2fe710a3
Ryan Pham <phamcake@gmail.com>
2021-12-05 14:48:32 -0800

    2021 day 1 part 2

--- 2021/day01/src/main.rs
+++ 2021/day01/src/main.rs
@@ -4,8 +4,7 @@ fn solve_part1() -> i32 {
     let input = read_input();
     let mut prev = None;
     let mut count = 0;
-    for line in input.lines() {
-        let curr = line.parse::<i32>().unwrap();
+    for curr in input.lines().map(|x| x.parse::<i32>().unwrap()) {
         if let Some(prev_val) = prev {
             if curr > prev_val {
                 count += 1;
@@ -17,11 +16,24 @@ fn solve_part1() -> i32 {
 }
 
 fn solve_part2() -> i32 {
+    let input = read_input();
+    let depths: Vec<i32> = input.lines().map(|x| x.parse::<i32>().unwrap()).collect();
+
     let mut count = 0;
+    let mut prev = None;
+    for curr_window in depths.windows(3) {
+        let curr = curr_window.iter().sum::<i32>();
+        if let Some(prev_sum) = prev {
+            if curr > prev_sum {
+                count += 1;
+            }
+        }
+        prev = Some(curr);
+    }
     count
 }
 
 fn main() {
-    println!("{}", solve_part1());
-    println!("{}", solve_part2());
+    println!("part 1: {}", solve_part1());
+    println!("part 2: {}", solve_part2());
 }
