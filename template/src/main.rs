898dd295896d96a8dcb5a93240d2776b9c039c8f
Ryan Pham <phamcake@gmail.com>
2021-12-05 14:58:18 -0800

    add daily template

---
+++ template/src/main.rs
@@ -0,0 +1,16 @@
+use common::read_input;
+
+fn solve_part1() -> i32 {
+    let mut ret = 0;
+    ret
+}
+
+fn solve_part2() -> i32 {
+    let mut ret = 0;
+    ret
+}
+
+fn main() {
+    println!("part 1: {}", solve_part1());
+    println!("part 2: {}", solve_part2());
+}
