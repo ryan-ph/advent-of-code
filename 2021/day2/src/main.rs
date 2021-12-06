667f4a2a4c912456e097914aecb6eab1b1c2bd2a
Ryan Pham <phamcake@gmail.com>
2021-12-05 16:04:05 -0800

    day 2 part 1

---
+++ 2021/day2/src/main.rs
@@ -0,0 +1,31 @@
+use common::read_input;
+
+fn solve_part1() -> i32 {
+    let mut horizontal = 0;
+    let mut vert = 0;
+
+    let input = read_input();
+    for line in input.lines() {
+        if let [action, unit_str] = line.split_whitespace().collect::<Vec<&str>>()[..] {
+            let unit = unit_str.parse::<i32>().unwrap();
+            if action == "forward" {
+                horizontal += unit;
+            } else if action == "down" {
+                vert += unit;
+            } else {
+                vert -= unit;
+            }
+        }
+    }
+    horizontal * vert
+}
+
+fn solve_part2() -> i32 {
+    let input = read_input();
+    0
+}
+
+fn main() {
+    println!("part 1: {}", solve_part1());
+    println!("part 2: {}", solve_part2());
+}
