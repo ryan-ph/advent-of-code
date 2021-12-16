0b6d48605536bd012daf24962a83f06cd5a39ba5
Ryan Pham <phamcake@gmail.com>
2021-12-15 20:14:42 -0800

    2020-day1: part 2

--- 2020/day1/src/main.rs
+++ 2020/day1/src/main.rs
@@ -1,5 +1,5 @@
 use common::read_input;
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
 fn solve_part1() -> i32 {
     let mut vals: HashSet<i32> = HashSet::new();
@@ -14,8 +14,28 @@ fn solve_part1() -> i32 {
 }
 
 fn solve_part2() -> i32 {
-    let mut ret = 0;
-    ret
+    let nums: Vec<i32> = read_input()
+        .lines()
+        .map(|x| x.parse::<i32>().unwrap())
+        .collect();
+
+    let mut vals: HashMap<i32, usize> = HashMap::new();
+    for (i, num) in nums.iter().enumerate() {
+        vals.insert(2020 - num, i);
+    }
+
+    for (i, num1) in nums.iter().enumerate() {
+        for (j, num2) in nums.iter().enumerate() {
+            let sum: i32 = num1 + num2;
+            if let Some(k) = vals.get(&sum) {
+                if i == j || i == *k || j == *k {
+                    continue;
+                }
+                return num1 * num2 * (2020 - sum);
+            }
+        }
+    }
+    -1
 }
 
 fn main() {
